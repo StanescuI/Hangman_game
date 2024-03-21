62fcde1a302cf72bd781d31a34c3bbc991de4b7d
StanescuI <162870985+StanescuI@users.noreply.github.com>
2024-03-21 11:19:53 +0200

    QOL update and finishing base functions
    Added the winning state
    fixed input to only take a character at a time
    input will be cleared after each letter
    input disappears when entering win / lose state

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function saveTheWord() {
     lives.innerHTML = livesLeft + "&#x2764;&#xfe0f;";
     let typedWord = document.getElementById("hangmanWord").value;
     word = typedWord;
-    var wordLength = word.length;
+    let wordLength = word.length;
     for (let i = 0; i < wordLength; ++i) {
         newText += " _"
     };
@@ -16,18 +16,28 @@ function saveTheWord() {
     element.parentNode.removeChild(element);
     result.innerHTML = "Type a single letter above to begin";
 }
+let guessedLetters = 0;
 function checkLetter() {
+    let wordLength = word.length;
     let myLetter = document.getElementById("writeLetter").value;
     let indexOfLetter = word.indexOf(myLetter);
     if (indexOfLetter >= 0) {
-        while (indexOfLetter >= 0 && indexOfLetter <= word.length) {
-            word = word.substring(0, indexOfLetter) + `.` + word.substring(indexOfLetter + 1);
-            newText = newText.substring(0,indexOfLetter * 2 + 1) + `${myLetter}` + newText.substring(indexOfLetter * 2 + 2);
-            document.getElementById("textZone").innerHTML = newText;
-            console.log(indexOfLetter);
-            indexOfLetter = word.indexOf(myLetter);
+        while (indexOfLetter >= 0) {
+            if (guessedLetters < wordLength) {
+                word = word.substring(0, indexOfLetter) + `.` + word.substring(indexOfLetter + 1);
+                newText = newText.substring(0,indexOfLetter * 2 + 1) + `${myLetter}` + newText.substring(indexOfLetter * 2 + 2);
+                document.getElementById("textZone").innerHTML = newText;
+                console.log(indexOfLetter);
+                guessedLetters += 1;
+                console.log(guessedLetters);
+                indexOfLetter = word.indexOf(myLetter);
+            } if (word.length === guessedLetters) {
+                const gameOver = document.getElementById("letterElements");
+                gameOver.parentNode.removeChild(gameOver);
+                result.innerHTML = "You won ! Press F5 to start again !";
+            }
+            document.getElementById("writeLetter").value = "";
         };
-        result.innerHTML = "Nice! The letter " + myLetter + " was found";
     } else {wrongLetter(myLetter)};
 }
 function wrongLetter(userLetter) {
